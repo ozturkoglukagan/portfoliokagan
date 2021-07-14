6ddc7455a1081374c148c2ff6cf43ff2d80cabc4
Kagan Ozturkoglu <ozturkoglukagan@gmail.com>
2021-07-14 23:25:57 +0300

    port changed

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ res.sendFile("index.html",{root: path.resolve()})
 
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
